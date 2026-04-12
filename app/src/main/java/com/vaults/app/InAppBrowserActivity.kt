b1a8e09472e29a5e49d9c6e9d8da41708280411d
Med-amine0 <medamine0@example.com>
2026-04-12 04:32:11 +0100

    Add: mute/unmute button, 5% default, PornHub landscape fullscreen, RedGif 2-click flow, random in fullscreen, InAppBrowser back fix

--- app/src/main/java/com/vaults/app/InAppBrowserActivity.kt
+++ app/src/main/java/com/vaults/app/InAppBrowserActivity.kt
@@ -36,10 +36,6 @@ class InAppBrowserActivity : AppCompatActivity() {
     }
 
     override fun onBackPressed() {
-        if (binding.webView.canGoBack()) {
-            binding.webView.goBack()
-        } else {
-            finish()
-        }
+        finish()
     }
 }
